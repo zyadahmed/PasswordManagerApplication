a86121194221f972db4655ddb73af8f027b2fa09
Ziad Ahmed <zyadahmed2466@gmail.com>
2024-07-10 21:19:00 +0300

    add doppler key store for storing secret credintails

---
+++ src/main/java/com/example/passwordmanager/Services/EncryptionService.java
@@ -0,0 +1,40 @@
+package com.example.passwordmanager.Services;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+@Service
+public class EncryptionService {
+    @Value("${AES_KEY}")
+    private String rawKey;
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init(){
+        byte[] decodedKey = Base64.getDecoder().decode(rawKey);
+        this.secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+
+    }
+    public String encrypt(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE,secretKey);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+    public String decrypt(String encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
+
+
+}
